180a8542c8a0b9622a9ff733da36513222e7107d
MadMaX-74 <maximus3506@mail.ru>
2020-02-19 21:47:36 +0300

    не работает

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+'use strict';
+
+
+const DATA ={
+    whichSite:['landing', 'multiPage', 'onlineStore'],
+    price:[4000,8000,26000],
+    desktopTemplates:[50,40,30],
+    adapt: 20,
+    mobileTemplates: 15,
+    editable: 10,
+    metrikaYandex:[500, 1000, 2000],
+    analyticsGoogle:[850, 1350, 3000],
+    sendOrder: 500,
+    deadlineDay:[[2, 7],[3, 10],[7, 14]],
+    deadlinePercent:[20, 17, 15]
+};
 
 const startButton = document.querySelector('.start-button'),
     firstScreen = document.querySelector('.first-screen'),
@@ -7,16 +23,35 @@ const startButton = document.querySelector('.start-button'),
     total = document.querySelector('.total'),
     fastRange = document.querySelector('.fast-range');
 
+    
 function showElem(elem) {
     elem.style.display = 'block';
+
 }
+
 function hideElem(elem) {
     elem.style.display = 'none';
 }
-function handlerCallBackForm() {
-    const target = event.target;
+
+function priceCalculation(elem) {
+    if (elem.name === 'whichSite') {
+        for (const item of formCalculate.elements){
+            if (item.type === 'checkbox') {
+                item.checked = false;
+            }
+        }
+        hideElem(fastRange);
+    }
+}
+
+function handlerCallBackForm(event) {
+    let target = event.target;
     if (target.classList.contains('want-faster')) {
-        target.checked ? showElem(fastRange): hideElem(fastRange);
+        target.checked ? showElem(fastRange) : hideElem(fastRange);
+    }
+
+    if (target.classList.contains('calc-hander')){
+        priceCalculation(target);
     }
 }
 
