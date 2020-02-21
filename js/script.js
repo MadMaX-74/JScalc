903d985003554a0eb56f86e2bddb900f8e436338
MadMaX-74 <maximus3506@mail.ru>
2020-02-21 19:27:44 +0300

    HOmework 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 
+const DAY_STRING = ['день','дня','дней'];
+
 const DATA ={
     whichSite:['landing', 'multiPage', 'onlineStore'],
     price:[4000,8000,26000],
@@ -24,8 +26,23 @@ const startButton = document.querySelector('.start-button'),
     fastRange = document.querySelector('.fast-range'),
     totalPriceSum = document.querySelector('.total_price__sum'),
     adapt = document.getElementById('adapt'),
-    mobileTemplates = document.getElementById('mobileTemplates');
+    mobileTemplates = document.getElementById('mobileTemplates'),
+    desktopTemplates = document.getElementById('desktopTemplates'),
+    editable = document.getElementById('editable'),
+    adaptValue = document.querySelector('.adapt_value'),
+    mobileTemplatesValue = document.querySelector('.mobileTemplates_value'),
+    desktopTemplatesValue = document.querySelector('.desktopTemplates_value'),
+    editableValue = document.querySelector('.editable_value'),
+    typeSite = document.querySelector('.type-site'),
+    maxDeadline = document.querySelector('.max-deadline'),
+    rangeDeadline = document.querySelector('.range-deadline'),
+    deadlineValue = document.querySelector('.deadline-value');
 
+
+function declOfNum(n, titles) {
+    return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+        0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}    
     
 function showElem(elem) {
     elem.style.display = 'block';
@@ -36,10 +53,29 @@ function hideElem(elem) {
     elem.style.display = 'none';
 }
 
+function renderTextContent(total, site, maxDay, minDay) {
+
+    totalPriceSum.textContent = total;
+    typeSite.textContent = site;
+    maxDeadline.textContent = declOfNum(maxDay, DAY_STRING);
+    rangeDeadline.min = minDay;
+    rangeDeadline.max = maxDay;
+    deadlineValue.textContent = declOfNum(rangeDeadline.value, DAY_STRING);
+
+    adaptValue.textContent = adapt.checked ? 'Да' : 'Нет';
+    mobileTemplatesValue.textContent = mobileTemplates.checked ? 'Да' : 'Нет';
+    desktopTemplatesValue.textContent = desktopTemplates.checked ? 'Да' : 'Нет';
+    editableValue.textContent = editable.checked ? 'Да' : 'Нет';
+}
+
 function priceCalculation(elem) {
     let result = 0,
         index = 0,
-        options = [];
+        options = [],
+        site = '',
+        maxDeadlineDay = DATA.deadlineDay[index][1],
+        minDeadlineDay = DATA.deadlineDay[index][0],
+        overPercent = 0;
 
     if (elem.name === 'whichSite') {
         for (const item of formCalculate.elements){
@@ -52,11 +88,19 @@ function priceCalculation(elem) {
     
     for (const item of formCalculate.elements){
         if (item.name === 'whichSite' && item.checked) {
-            index = DATA.whichSite.indexOf(item.value);            
+            index = DATA.whichSite.indexOf(item.value);
+            site = item.dataset.site; 
+            maxDeadlineDay = DATA.deadlineDay[index][1];
+            minDeadlineDay = DATA.deadlineDay[index][0];           
         }else if (item.classList.contains('calc-handler') && item.checked){
             options.push(item.value);
+        }else if (item.classList.contains('want-faster') && item.checked){
+            const overDay = maxDeadlineDay - rangeDeadline.value;
+            overPercent = overDay * DATA.deadlinePercent[index]/100;
         }
     }
+
+    result += DATA.price[index];
     
     options.forEach(function(key){
         if (typeof(DATA[key]) === 'number') {
@@ -72,15 +116,18 @@ function priceCalculation(elem) {
                 result += DATA[key][index]; 
             }
         }
-    })
+    });    
 
-    result += DATA.price[index];
+    result += result * overPercent;
 
-    totalPriceSum.textContent = result;
+    renderTextContent(result, site, maxDeadlineDay, minDeadlineDay);
+
+    
 }
 
 function handlerCallBackForm(event) {
     let target = event.target;
+    
 
     if (adapt.checked) {
         mobileTemplates.disabled = false;   
@@ -91,6 +138,7 @@ function handlerCallBackForm(event) {
 
     if (target.classList.contains('want-faster')) {
         target.checked ? showElem(fastRange) : hideElem(fastRange);
+        priceCalculation(target);
     }
 
     if (target.classList.contains('calc-handler')) {
